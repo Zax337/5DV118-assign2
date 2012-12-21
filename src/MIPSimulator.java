7bf80f0428d8c7976574e7efe03ef6d7a68641a2
AcidFlow <laturaze.paul@gmail.com>
2012-12-21 15:57:46 +0100

    Main class

---
+++ src/MIPSimulator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Acid Flow
+ *
+ */
+public final class MIPSimulator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
